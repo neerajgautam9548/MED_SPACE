0fcdd83a61b7b982e4c8b1712d153abcb5d1ac9e
neerajgautam9548 <neerajgautam9548@gmail.com>
2025-12-06 08:09:21 +0530

    Delete client directory

--- client/src/store/RegistrationContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const RegistrationContext = createContext({});
-
-export default RegistrationContext;
-
-export const { Provider, Consumer } = RegistrationContext;
--- client/src/store/atom.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const mode = atom({
-    key: 'mode',
-    default: 'light',
-});
